64c712fbdb60fa79ebe3486a0f6c17e52d55e7ff
nakkekakke <juntusen.niko@gmail.com>
2019-09-24 11:39:34 +0300

    combine lines

---
+++ Mobiilishakki/app/src/main/java/fi/helsinki/mobiilishakki/Line.java
@@ -0,0 +1,75 @@
+package fi.helsinki.mobiilishakki;
+
+import org.opencv.core.Point;
+
+/**
+ * Class representing line.
+ */
+public class Line {
+
+    private double rho;
+    private double theta;
+
+    /**
+     * Constructor for Line-object.
+     * @param rho
+     * @param theta
+     */
+    public Line(double rho, double theta) {
+        this.rho = rho;
+        this.theta = theta;
+    }
+
+    /**
+     * Get rho value from polar coordinate system.
+     * r = x*cos(t) + y*sin(t)
+     * @return rho
+     */
+    public double getRho() {
+        return rho;
+    }
+
+    /**
+     * Get theta value from polar coordinate system.
+     * @return theta
+     */
+    public double getTheta() {
+        return theta;
+    }
+
+    /**
+     * Get slope m of the line. Line function is y = m*x + c
+     * @return m
+     */
+    public double getSlope() {
+        return -Math.cos(theta)/Math.sin(theta);
+    }
+
+    /**
+     * Get constant c from line function y = m*x + c.
+     * @return c
+     */
+    public double getConstant() {
+        return rho/Math.sin(theta);
+    }
+
+    /**
+     * Get line starting point.
+     * @return point
+     */
+    public Point getStartingPoint() {
+        double a = Math.cos(theta), b = Math.sin(theta);
+        double x0 = a * rho, y0 = b * rho;
+        return new Point(Math.round(x0 + 10000 * (-b)), Math.round(y0 + 10000 * (a)));
+    }
+
+    /**
+     * Get line ending point.
+     * @return ending point
+     */
+    public Point getEndingPoint() {
+        double a = Math.cos(theta), b = Math.sin(theta);
+        double x0 = a * rho, y0 = b * rho;
+        return new Point(Math.round(x0 - 10000 * (-b)), Math.round(y0 - 10000 * (a)));
+    }
+}
--- Mobiilishakki/app/src/main/java/fi/helsinki/mobiilishakki/MainActivity.java
+++ Mobiilishakki/app/src/main/java/fi/helsinki/mobiilishakki/MainActivity.java
@@ -25,23 +25,43 @@ import org.opencv.core.*;
 import org.opencv.calib3d.Calib3d.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
+import static org.opencv.calib3d.Calib3d.CALIB_CB_ADAPTIVE_THRESH;
+import static org.opencv.calib3d.Calib3d.CALIB_CB_FAST_CHECK;
+import static org.opencv.calib3d.Calib3d.CALIB_CB_NORMALIZE_IMAGE;
+import static org.opencv.calib3d.Calib3d.drawChessboardCorners;
+import static org.opencv.calib3d.Calib3d.findChessboardCorners;
 import static org.opencv.core.Core.BORDER_DEFAULT;
+import static org.opencv.core.Core.LINE_AA;
 import static org.opencv.core.CvType.CV_8UC3;
 import static org.opencv.imgproc.Imgproc.CHAIN_APPROX_SIMPLE;
 import static org.opencv.imgproc.Imgproc.COLOR_BGR2GRAY;
+import static org.opencv.imgproc.Imgproc.Canny;
 import static org.opencv.imgproc.Imgproc.GaussianBlur;
+import static org.opencv.imgproc.Imgproc.HoughLines;
 import static org.opencv.imgproc.Imgproc.LINE_8;
 import static org.opencv.imgproc.Imgproc.RETR_TREE;
+import static org.opencv.imgproc.Imgproc.THRESH_BINARY;
 import static org.opencv.imgproc.Imgproc.approxPolyDP;
 import static org.opencv.imgproc.Imgproc.arcLength;
 import static org.opencv.imgproc.Imgproc.contourArea;
+import static org.opencv.imgproc.Imgproc.cornerSubPix;
+import static org.opencv.imgproc.Imgproc.cvtColor;
 import static org.opencv.imgproc.Imgproc.drawContours;
+import static org.opencv.imgproc.Imgproc.findContours;
 import static org.opencv.imgproc.Imgproc.isContourConvex;
+import static org.opencv.imgproc.Imgproc.line;
+import static org.opencv.imgproc.Imgproc.threshold;
 
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static int frameWidth = 0;
+    private static int frameHeight = 0;
 
     // Latest frame that was processed
     private Mat lastFrame;
@@ -97,66 +117,352 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         // Mat object for holding rgb frame
         Mat rgbFrame = inputFrame.rgba();
 
-        GaussianBlur(rgbFrame, rgbFrame, new Size(5, 5), BORDER_DEFAULT);
-
         // Mat object for holding gray frame
         Mat grayFrame = new Mat();
-        Imgproc.cvtColor(rgbFrame, grayFrame, COLOR_BGR2GRAY);
+        cvtColor(rgbFrame, grayFrame, COLOR_BGR2GRAY);
+
+        Mat binaryMat = new Mat(grayFrame.size(), grayFrame.type());
+
+        //Apply thresholding
+        threshold(grayFrame, binaryMat, 100, 255, THRESH_BINARY);
+
+        // Blurring the image to reduce the amount of "false positives"
+        GaussianBlur(grayFrame, grayFrame, new Size(3, 3), BORDER_DEFAULT);
+
         // Mat object for holding the result of edge detection (Canny)
         Mat edges = new Mat();
+        Canny(grayFrame, edges, 50, 150, 3, false);
 
-        //int size = (int)grayFrame.total() * grayFrame.channels();
-        //byte[] array = new byte[size];
+        // Apply line detection
+        Mat linesMat = new Mat();
+        HoughLines(edges, linesMat, 1, Math.PI / 180, 150);
 
-        //InputArray arrayy = new InputArray();
-        //grayFrame.get(0, 0, array);
-
-        //Calib3d.findChessboardCorners(array);
-
-        Imgproc.Canny(grayFrame, edges, 50, 150, 3, false);
-
-        // Find contours from the frame
-        List<MatOfPoint> contours = new ArrayList<>();
-        Mat hierarchy = new Mat();
-        Imgproc.findContours(edges, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
-        MatOfPoint2f approxCurve = new MatOfPoint2f();
-
-        // performance boost --> if small amount of contours --> cant be chessboard
-        if(contours.size() < 40) {
+        // Skip if no lines found
+        if (linesMat.empty()) {
+            this.lastFrame = rgbFrame;
             return rgbFrame;
         }
 
-        drawContours(rgbFrame, contours, -1, new Scalar(255, 0, 0), 1, 8, hierarchy);
+        // Get lines in a list
+        List<Line> lines = getLinesList(linesMat);
 
-        // for each contour found
-        /*for (int i = 0; i < contours.size(); i++){
+        // Get linegroups
+        List<Linegroup> linegroups = getLinegroups(lines);
 
-            MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
-            double approxDistance = arcLength(contour2f, true) * 0.02;
+        // Get merged lines
+        List<Line> mergedLines = mergeLines(linegroups);
 
-            if(approxDistance > 1){
-                approxPolyDP(contour2f, approxCurve, approxDistance, true);
-                MatOfPoint points = new MatOfPoint(approxCurve.toArray());
+        // Keep merging lines until stable situation
+        while (lines.size() != mergedLines.size()) {
+            lines = mergedLines;
+            linegroups = getLinegroups(lines);
+            mergedLines = mergeLines(linegroups);
+        }
 
-                // Rectangle checks
-                if (points.total() == 4 && Math.abs(contourArea(points)) > 1000 && isContourConvex(points)){
-                    double cos = 0;
-                    double mcos = 0;
-                    for(int sc = 2; sc < 5; sc++) {
-                        if(cos > mcos) {
-                            mcos = cos;
-                        }
-                    }
-                    if(mcos < 0.3) {
+        // Filter lines that are not part of main chessboard grid
+        lines = filterRedundantLines(mergedLines);
 
-                        drawContours(rgbFrame, contours, i, new Scalar(255, 0, 0), 2, LINE_8, hierarchy, 0, new Point());
-                    }
-                }
-            }
-        }*/
+        // Check if we have right amount of lines to form chessboard
+        if (lines.size() == 18) {
+            System.out.println("Could be a board!!!");
+        }
+
+        // Draw lines to video feed
+        drawLinesToMat(lines, rgbFrame);
+
         this.lastFrame = rgbFrame;
         return rgbFrame;
     }
+
+
+    /**
+     * Try to recognise grid from frame and write grid lines to picture.
+     */
+    private static Mat recogniseBoardGrid(Mat frame) {
+        // Update frame size information to variables
+        frameWidth = frame.width();
+        frameHeight = frame.height();
+
+        // Mat object for holding the new binary image
+        Mat gray = new Mat();
+        cvtColor(frame, gray, COLOR_BGR2GRAY);
+
+        // Blur the frame
+        GaussianBlur(gray, gray, new Size(3, 3), BORDER_DEFAULT);
+
+        // Mat object for holding the result of edge detection (Canny)
+        Mat edges = new Mat();
+        Canny(gray, edges, 50, 150, 3, false);
+
+        // Apply line detection
+        Mat linesMat = new Mat();
+        HoughLines(edges, linesMat, 1, Math.PI / 180, 150);
+
+        if (linesMat.empty()) {
+            return null;
+        }
+
+        // Get lines in a list
+        List<Line> lines = getLinesList(linesMat);
+        // Get linegroups
+        List<Linegroup> linegroups = getLinegroups(lines);
+        // Get merged lines
+        List<Line> mergedLines = mergeLines(linegroups);
+
+        // Keep merging lines until stable situation
+        while (lines.size() != mergedLines.size()) {
+            lines = mergedLines;
+            // Get linegroups
+            linegroups = getLinegroups(lines);
+            // Get merged lines
+            mergedLines = mergeLines(linegroups);
+        }
+
+        // Filter lines that are not part of main chessboard grid
+        lines = filterRedundantLines(mergedLines);
+
+        // Check if we right amount of lines to form chessboard
+        if (mergedLines.size() == 18) {
+            System.out.println("Could be a board!!!");
+        }
+
+        // Draw lines to frame
+        Mat grid = Mat.zeros(edges.size(), CV_8UC3);
+        grid = drawLinesToMat(mergedLines, grid);
+
+        // Detect contours from grid
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        cvtColor(grid, grid, COLOR_BGR2GRAY);
+        findContours(grid, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+
+        //Mat res = Mat.zeros(edges.size(), CV_8UC1);
+        drawContours(gray, contours, -1, new Scalar(255, 0, 0), 1, LINE_AA);
+
+        // Return the modified frame where the redundant lines are filtered
+        return gray;
+    }
+
+    /**
+     * Draw lines to Mat-object.
+     */
+    private static Mat drawLinesToMat(List<Line> mergedLines, Mat frame) {
+        for (Line line : mergedLines) {
+            double a = Math.cos(line.getTheta()), b = Math.sin(line.getTheta());
+            double x0 = a * line.getRho(), y0 = b * line.getRho();
+            Point pt1 = new Point(Math.round(x0 + 10000 * (-b)), Math.round(y0 + 10000 * (a)));
+            Point pt2 = new Point(Math.round(x0 - 10000 * (-b)), Math.round(y0 - 10000 * (a)));
+            line(frame, pt1, pt2, new Scalar(0, 0, 255), 1, LINE_AA, 0);
+        }
+        return frame;
+    }
+
+    /**
+     * Get List of lines from Mat-object.
+     */
+    private static List<Line> getLinesList(Mat lines) {
+        List<Line> lineList = new ArrayList<>();
+        for (int i = 0; i < lines.rows(); i++) {
+            double rho = lines.get(i, 0)[0];
+            double theta = lines.get(i, 0)[1];
+            Line line = new Line(rho, theta);
+            lineList.add(line);
+        }
+        return lineList;
+    }
+
+    /**
+     * Get List of merged lines.
+     */
+    private static List<Line> mergeLines(List<Linegroup> linegroups) {
+        // Create list to contain information about all merged lines
+        List<Line> mLines = new ArrayList<>();
+        // Iterate through all linegroups and combine groups
+        for (Linegroup linegroup : linegroups) {
+            double rho = linegroup.getAverageRho();
+            double theta = linegroup.getAverageTheta();
+            Line line = new Line(rho, theta);
+            mLines.add(line);
+        }
+        return mLines;
+    }
+
+    /**
+     * Get List of linegroups.
+     */
+    private static List<Linegroup> getLinegroups(List<Line> lines) {
+        // Define thresholds for rho and theta
+        double rhoThreshold = 30;
+        double thetaThreshold = 0.1;
+
+        // HashMap to keep track of group for each line <line index, group number>
+        HashMap<Integer, Integer> groupMap = new HashMap<>();
+        // List of all linegroups
+        List<Linegroup> linegroups = new ArrayList<>();
+
+        // Compare every line to every other line and find the linegroup of the most similar line.
+        // Most similar line is considered to be the one with closest rho value. However, the difference
+        // between theta values must be under threshold value, so that the lines can be considered the same line.
+        int linegroupNumber = 0;    // next available linegroup number
+        for (int i = 0; i < lines.size(); i++) {
+            int msIndx = -1;
+            double rhoDif = Double.MAX_VALUE;
+            double rhoI = lines.get(i).getRho(), thetaI = lines.get(i).getTheta();
+            for (int j = 0; j < lines.size(); j++) {
+                if (i == j) {
+                    continue;   // Do not compare line to itself
+                }
+                // Get rho and theta from each lines
+                double rhoJ = lines.get(j).getRho(), thetaJ = lines.get(j).getTheta();
+                // Check if lines are similar enough to be considered the same line
+                double difR = Math.abs(rhoI - rhoJ);
+                double difT = Math.abs(thetaI - thetaJ);
+                if (difR < rhoThreshold && difT < thetaThreshold) {
+                    if (difR < rhoDif) {
+                        rhoDif = difR;
+                        msIndx = j;
+                    }
+                }
+            }
+
+            // Set group for i:th line to be the same as the group of the most similar line.
+            // If there is no most similar line, then create new linegroup.
+            if (msIndx == -1 || groupMap.get(msIndx) == null) {
+                groupMap.put(i, linegroupNumber);
+                groupMap.put(msIndx, linegroupNumber);
+                linegroupNumber++;
+                linegroups.add(new Linegroup());
+                linegroups.get(linegroups.size() - 1).addLine(lines.get(i));
+            } else {
+                int groupNum = groupMap.get(msIndx);
+                groupMap.put(i, groupNum);
+                linegroups.get(groupNum).addLine(lines.get(i));
+            }
+        }
+        // Return linegroups
+        return linegroups;
+    }
+
+    /**
+     * Transform Mat-object tot BufferedImage-object.
+     */
+/*    private static BufferedImage Mat2BufferedImage(Mat m) {
+        //Method converts a Mat to a Buffered Image
+        int type = BufferedImage.TYPE_BYTE_GRAY;
+        if (m.channels() > 1) {
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        int bufferSize = m.channels() * m.cols() * m.rows();
+        byte[] b = new byte[bufferSize];
+        m.get(0, 0, b); // get all the pixels
+        BufferedImage image = new BufferedImage(m.cols(), m.rows(), type);
+        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        System.arraycopy(b, 0, targetPixels, 0, b.length);
+        return image;
+    }*/
+
+    /**
+     * Filter lines that are not part of the chessboard grid.
+     * In other words, get rid of redundant lines.
+     */
+    private static List<Line> filterRedundantLines(List<Line> lines) {
+        // Create lists for vertical and horizontal lines
+        List<Line> vertical = new ArrayList<>();
+        List<Line> horizontal = new ArrayList<>();
+        // Iterate through all lines and group them to horizontal and vertical lines
+        for(Line line : lines) {
+            if(line.getTheta() >= Math.PI/4 && line.getTheta() <= Math.PI/4*3) {
+                vertical.add(line);     // line angle closer to vertical
+            }else {
+                horizontal.add(line);   // line angle closer to horizontal
+            }
+        }
+
+        // TODO: remove lines that overlap in frame area
+        vertical = removeIntersectingLines(vertical, true);
+        horizontal = removeIntersectingLines(horizontal, false);
+
+        // TODO: remove extra lines.
+        //  9 most centered lines should be considered as actual grid lines
+
+        // TODO: merge horizontal and vertical lines to
+        return lines;
+    }
+
+    /**
+     * Remove lines that intersect each other in frame area. The line that has higher
+     */
+    private static List<Line> removeIntersectingLines(List<Line> lines, boolean isVertical) {
+        // Array where deleted lines are marked
+        boolean[] deletedLines = new boolean[lines.size()];
+        // Iterate through all combinations
+        for(int i = 0; i < lines.size(); i++) {
+            if(deletedLines[i] == true) {
+                continue;
+            }
+            for(int j = 0; j < lines.size(); j++) {
+                if(i == j || deletedLines[j] == true ||deletedLines[i] == true) {
+                    continue;
+                }
+                // Check if lines intersect
+                Line lineI = lines.get(i);
+                Line lineJ = lines.get(j);
+                if(linesIntersect(lineI, lineJ)) {
+                    // Remove the one that is less optimal
+                    double deltaI = Double.MAX_VALUE;
+                    double deltaJ = Double.MAX_VALUE;
+                    if(isVertical) {
+
+                    }else{
+
+                    }
+                    if(deltaI > deltaJ) {
+                        deletedLines[i] = true;
+                    }else {
+                        deletedLines[j] = true;
+                    }
+                }
+            }
+        }
+        // Return lines that do not overlap each other in frame area
+        return lines;
+    }
+
+    /**
+     * Check if lines intersect in frame area.
+     */
+    private static boolean linesIntersect(Line line1, Line line2) {
+
+        // Get start and end points for lines
+        Point p1 = line1.getStartingPoint();
+        Point p2 = line1.getEndingPoint();
+        Point p3 = line2.getStartingPoint();
+        Point p4 = line2.getEndingPoint();
+
+        double a1 = p2.y - p1.y;
+        double b1 = p1.x - p2.x;
+        double c1 = a1 * p1.x + b1 * p1.y;
+
+        double a2 = p4.y - p3.y;
+        double b2 = p3.x - p4.x;
+        double c2 = a2 * p3.x + b2 * p3.y;
+
+        double determinant = a1 * b2 - a2 * b1;
+
+        // if determinant is zero --> lines do not intersect
+        if(determinant == 0) {
+            return false;
+        }
+
+        double x = (b2*c1 - b1*c2)/determinant;
+        double y = (a1*c2 - a2*c1)/determinant;
+
+        if(x >= 0 && x <= frameWidth && y >= 0 && y <= frameHeight) {
+            return true;
+        }
+        return false;
+    }
+
 
     @Override
     protected void onResume() {
